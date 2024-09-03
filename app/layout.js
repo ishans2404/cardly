0475620d8fb3aac137e5d18dd534010f7cb80557
ishans2404 <ishans2404@gmail.com>
2024-09-04 00:17:18 +0530

    adding google analytics

--- app/layout.js
+++ app/layout.js
@@ -3,7 +3,7 @@ import "./globals.css";
 import { ClerkProvider } from "@clerk/nextjs";
 import { ThemeProvider, CssBaseline } from '@mui/material';
 import theme from '../theme';
-import Script from "next/script";
+import { GoogleAnalytics } from '@next/third-parties/google';
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -14,26 +14,16 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <>
-    <Script
-      id='gtm'
-      strategy='afterInteractive'
-      dangerouslySetInnerHtml={{
-        __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-    new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-    j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-    'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-    })(window,document,'script','dataLayer','GTM-TGPKWNC2');`
-      }}
-    />
-    <ClerkProvider>
-      <html lang="en">
+    <html lang="en">
+      <ClerkProvider>
         <ThemeProvider theme={theme}>
           <CssBaseline />
-          <body className={inter.className}>{children}</body>
+          <body className={inter.className}>
+            {children}
+            <GoogleAnalytics gaId="G-FEP1TF34BN" />
+          </body>
         </ThemeProvider>
-      </html>
-    </ClerkProvider>
-    </>
+      </ClerkProvider>
+    </html>
   );
 }
